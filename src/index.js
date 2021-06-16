3051c81e4091ce485c8503943a7423296e9eb96b
Scott Bailey <csb5t@virginia.edu>
2021-06-16 16:30:32 -0400

    Add minimal working mirador annotations with localStorage; creating an anno doesn't seem to work yet though, and I could import the LocalStorageAdapter correctly from the package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
 import mirador from "mirador";
+import annotationPlugins from "mirador-annotations";
+// import { LocalStorageAdapter } from "mirador-annotations/es/LocalStorageAdapter";
+import LocalStorageAdapter from "./LocalStorageAdapter";
 
 class Mirador extends Component {
   componentDidMount() {
@@ -17,9 +19,28 @@ class Mirador extends Component {
 
 export default Mirador;
 
+const config = {
+  id: "mirador",
+  annotation: {
+    adapter: (canvasId) =>
+      new LocalStorageAdapter(`localStorage://?canvasId${canvasId}`),
+    exportLocalStorageAnnotations: false,
+  },
+  window: {
+    defaultSideBarPanel: "annotations",
+    sideBarOpenByDefault: true,
+  },
+  windows: [
+    {
+      loadedManifest:
+        "https://iiif.harvardartmuseums.org/manifests/object/299843",
+    },
+  ],
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <Mirador config={{ id: "mirador" }} plugins={[]} />
+    <Mirador config={config} plugins={[...annotationPlugins]} />
   </React.StrictMode>,
   document.getElementById("root")
 );
